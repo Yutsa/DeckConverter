9c4cbd9e9c849df3ec3e711b74cf93696e45606b
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-03-28 15:39:44 +0200

    The interface is complete. Added the choice of the card.cdb. Started to work on the converter itself

--- deckconverter/converter.py
+++ deckconverter/converter.py
@@ -11,6 +11,8 @@ class Converter:
         self.cardmarket = False
         self.price = False
         self.deckFile = ""
+        self.convList = ""
+        self.cardDB = ""
         
     def changePriceState(self):
         self.price = not self.price
@@ -25,3 +27,35 @@ class Converter:
     def changeDeckFile(self, file):
         self.deckFile = file
         logger.info("Deck file: " + file)
+
+    def changeConvList(self, file):
+        self.convList = file
+        logger.info("Saving as: " + str(file))
+
+    def changeCardDB(self, file):
+        self.cardDB = file
+        logger.info("cards.cdb : " + self.cardDB)
+
+    def convert(self):
+        print("Converting !")
+        with open(self.deckFile, 'r') as ydkList:
+            with open(self.convList, "w") as convertedList:
+                for line in ydkList:
+                    if self.markdown:
+                        if "#created" in line:
+                            convertedList.write("Created using " +
+                                                "DeckConverter\n\n")
+                        elif "#main" in line:
+                            convertedList.write("\nMain Deck: \n\n")
+                        elif "#extra" in line:
+                            convertedList.write("\nExtra Deck: \n\n")
+                        elif "!side" in line:
+                            convertedList.write("\nSide Deck: \n\n")
+                        else:
+                            if self.price:
+                                convertedList.write(line[:-1] +
+                                                    "- PRICE\n")
+                            else:
+                                convertedList.write(line)
+                    else:
+                        convertedList.write("CARDMARKET -- " + line)
--- deckconverter/handler.py
+++ deckconverter/handler.py
@@ -1,4 +1,6 @@
 from deckconverter.converter import Converter
+from gi.repository import Gtk
+from deckconverter.logger import *
 
 class Handler:
     """
@@ -15,3 +17,25 @@ class Handler:
 
     def onClickFormat(self, formatButton):
         self.converter.changeFormat()
+
+    def onClickConvert(self, convertButton):
+        self.converter.convert()
+
+    def onClickCdb(self, cdbChooser):
+        self.converter.changeCardDB(cdbChooser.get_filename())
+
+    def onClickSaveAs(self, window):
+        dialog = Gtk.FileChooserDialog("Save as", window,
+                                       Gtk.FileChooserAction.SAVE,
+                                       (Gtk.STOCK_CANCEL,
+                                        Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OK,
+                                        Gtk.ResponseType.OK))
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            self.converter.changeConvList(dialog.get_filename())
+        else:
+            logger.info("Canceled the selection of the saving file.")
+
+        dialog.destroy()
